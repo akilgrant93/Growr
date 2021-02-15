11861476cad017739c7b6eeb3347c9c9492fdb54
Akil Grant <akil.grant.93@gmail.com>
2021-02-15 15:42:29 -0500

    basic push notification setup

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import Routes from './Components/Routes';
+import React, { useState, useEffect, useRef } from 'react'
+import { Platform } from 'react-native';
+import Constants from 'expo-constants';
+import * as Notifications from 'expo-notifications';
+import Routes from './Components/Routes'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import applyMiddleWare from 'redux/src/applyMiddleware'
@@ -10,13 +13,44 @@ import firebase from 'firebase'
 import Loading from './Components/Loading'
 import {decode, encode} from 'base-64'
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
 if (!global.btoa) {  global.btoa = encode }
 if (!global.atob) { global.atob = decode }
 
-// export default class App extends Component {
   export default function App(){
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [uid, setUid] = useState('');
+    const [expoPushToken, setExpoPushToken] = useState('');
+    const [notification, setNotification] = useState(false);
+    const notificationListener = useRef();
+    const responseListener = useRef();
+
+    useEffect(() => {
+      registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+
+      // This listener is fired whenever a notification is received while the app is foregrounded
+      notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+        setNotification(notification);
+      });
+
+      // This listener is fired whenever a user taps on or interacts with a notification (works when app is foregrounded, backgrounded, or killed)
+      responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+        console.log(response);
+      });
+
+      return () => {
+        Notifications.removeNotificationSubscription(notificationListener);
+        Notifications.removeNotificationSubscription(responseListener);
+      };
+    }, []);
 
     useEffect(() => {
       firebase.auth().onAuthStateChanged(user => {
@@ -28,12 +62,6 @@ if (!global.atob) { global.atob = decode }
         }
    })
      }, [])
-
-  // state={
-  //   //toggle true to render routes supposed to be triggered in the firebase.auth function in the componentDidMount
-  //   uid: '',
-  //   loggedIn: false
-  // }
 
   const renderContent = () => {
     switch(isLoggedIn){
@@ -55,4 +83,55 @@ if (!global.atob) { global.atob = decode }
       </Provider>
       </SafeAreaProvider>
     )
+}
+
+async function sendPushNotification(expoPushToken) {
+  const message = {
+    to: expoPushToken,
+    sound: 'default',
+    title: 'Original Title',
+    body: 'And here is the body!',
+    data: { someData: 'goes here' },
+  };
+
+  await fetch('https://exp.host/--/api/v2/push/send', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Accept-encoding': 'gzip, deflate',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(message),
+  });
+}
+
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Constants.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  return token;
 }
