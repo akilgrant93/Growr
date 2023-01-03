0568919ce4a4f915bfa8f6b780d997219b0cdd3d
Akil Grant <akil.grant.93@gmail.com>
2023-01-02 20:16:51 -0500

    scroll on changeDate added to calendar component

--- src/Calendar.js
+++ src/Calendar.js
@@ -12,12 +12,14 @@ const MyCalendar = () => {
   const navigation = useNavigation()
   const [wateringDays, setWateringDays] = useState([])
   const [nextWateringDays, setNextWateringDays] = useState([])
+  const [dateInfo, setDateInfo] = useState([])
   const [dates, setDates] = useState([])
   const [plants, setPlants] = useState([])
   const [date, setDate] = useState(moment().startOf('day'))
   const [lastWateredData, setLastWateredData] = useState({})
   const [nextWateredData, setNextWateredData] = useState([])
   const [toggled, setToggled] = useState(false)
+  const [ref, setRef] = useState(null);
 
   useFocusEffect(
     React.useCallback(() => {
@@ -52,12 +54,14 @@ const MyCalendar = () => {
 
     let range = 1 + moment(nextWateringDays[nextWateringDays.length-1]).diff(moment(wateringDays[0]).startOf('day'), 'days')
     let datesArr = []
+    let datesArr2 = []
 
     for(let i = 0; i < range; i++){
       let nextPlantsToWaterArr = []
       let previouslyWateredPlantsArr = []
 
       datesArr.push({date:moment(wateringDays[0]).startOf('day').add(i,'days')})
+      datesArr2.push(moment(wateringDays[0]).startOf('day').add(i,'days').toString())
 
       if(wateringDays.includes(moment(wateringDays[0]).startOf('day').add(i,'days').toString())){
         datesArr[i].previousWateringDay = true
@@ -80,7 +84,8 @@ const MyCalendar = () => {
       }
 
     }
-    setDates(datesArr)
+    setDates(datesArr2)
+    setDateInfo(datesArr)
     }, [])
   );
 
@@ -119,6 +124,23 @@ const customDatesStylesCallback = date => {
   }
 }
 
+const changeDate = (date) => {
+  //scroll to date
+  if(dates.includes(date.startOf('day').toString())){
+    console.log('included')
+    for(let i = 0; i < dates.length; i++){
+      if(dates[i] === date.startOf('day').toString()){
+        ref.scrollToIndex({animated: true, index: i})
+      }
+    }
+  } //if the date is before the beginning of the range of dates
+  else if (date.startOf('day').valueOf() < moment(dates[0]).valueOf()) {
+    ref.scrollToIndex({animated: true, index: 0})
+  } //if the date is after the end of the range of dates
+  else if (date.startOf('day').valueOf() > moment(dates[dates.length-1]).valueOf()) {
+    ref.scrollToIndex({animated: true, index: dates.length-1})
+  }
+}
 
   return (
     <SafeAreaView style={{alignItems:'center', paddingTop: '3%', height: '100%'}}>
@@ -130,6 +152,7 @@ const customDatesStylesCallback = date => {
             </View>
       <View style={{paddingBottom: 15, width: '90%',marginLeft:'5%'}}>
       <CalendarPicker
+      onDateChange={changeDate}
       width={Dimensions.get('window').width*.8}
       previousComponent={
         <View style={{backgroundColor:'rgba(255,255,255,.25)', width:75, marginLeft:-7.5,padding: 10,borderRadius: 25}}>
@@ -148,11 +171,11 @@ const customDatesStylesCallback = date => {
       </View>
         </View>
 
-      <View style={{height: '44.5%', backgroundColor:'rgba(249,112,104,.5)', borderRadius: 25}}>
+      <View style={{height: '44.5%', backgroundColor:'rgba(249,112,104,.5)', borderRadius: 25, overflow:'hidden'}}>
 
-      <View style={{flexDirection:'row', alignItems:'center', paddingTop:10, paddingTop:30, marginLeft: 15}}>
-                <Text style={{fontSize: 25, fontWeight: 'bold', color: '#fff'}}>Reminders</Text>
-                <FontAwesome
+          <View style={{flexDirection:'row', alignItems:'center', paddingTop:10, paddingTop:30, marginLeft: 15}}>
+              <Text style={{fontSize: 25, fontWeight: 'bold', color: '#fff'}}>Reminders</Text>
+              <FontAwesome
                 style={{marginLeft: 5}}
                 name='bell'
                 size={20}
@@ -161,8 +184,11 @@ const customDatesStylesCallback = date => {
             </View>
 
       <FlatList
+          ref={(ref) => {
+             setRef(ref);
+             }}
           showsVerticalScrollIndicator={false}
-          data={dates}
+          data={dateInfo}
           renderItem={({item, index}) => {
             return (
             <View key={index} style={[{paddingBottom: 40}, item.date.toString() === moment().startOf('day').toString() ? {backgroundColor:'rgba(249,112,104,.5)'}: null]}>
@@ -178,6 +204,9 @@ const customDatesStylesCallback = date => {
                       </View>
                   </View>
 
+                  {item.date.toString() === moment().startOf('day').toString() && item.nextPlantsToWater === undefined && item.previouslyWateredPlants === undefined ? <View key={index} style={{flexDirection:'row', marginBottom:7.5}}>
+                  <Text style={{fontWeight:'bold', opacity:0,paddingRight: '5%'}}>TODO</Text><Text style={{color:'#fff'}}>No tasks today</Text>
+                      </View> : null}
 
                   {item.nextPlantsToWater ? item.nextPlantsToWater.map((plant, index) => {
                     return (
@@ -187,9 +216,9 @@ const customDatesStylesCallback = date => {
                     )
                   }) : null}
 
-                  {item.previouslyWateredPlants ? item.previouslyWateredPlants.map((plant) => {
+                  {item.previouslyWateredPlants ? item.previouslyWateredPlants.map((plant, idx) => {
                     return (
-                      <View style={{flexDirection:'row', marginBottom:7.5}}>
+                      <View key={idx} style={{flexDirection:'row', marginBottom:7.5}}>
                         <Text style={{fontWeight:'bold', color:'#034732', paddingRight: '5%'}}>DONE</Text><Text style={{color:'white'}}>Water {plant.name}</Text>
                       </View>
                     )
