ca60ad0c46f85f164d2004fee91792ff25477cb8
Akil Grant <akil.grant.93@gmail.com>
2023-01-09 18:27:13 -0500

    visual styles for settings component completed - needs functionality for most of the functions and visuals to match

--- src/Settings.js
+++ src/Settings.js
@@ -1,6 +1,7 @@
-import { StyleSheet, Text, View,TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, View,TouchableOpacity, SafeAreaView } from 'react-native'
 import {firebase} from '../config'
 import React from 'react'
+import { FontAwesome } from '@expo/vector-icons'
 
 const Settings = () => {
   const changePassword = () => {
@@ -13,33 +14,150 @@ const Settings = () => {
   }
 
   return (
-    <View>
+    <View style={{height:'100%', backgroundColor:'#034732'}}>
+      <SafeAreaView style={{ height: '100%', backgroundColor:'rgba(255,255,255,.25)' }}>
+      <View style={{width: '90%', height: '97.5%', marginLeft:'5%',backgroundColor:'rgba(255,255,255,.25)', borderRadius: 25}}>
+      <View style={{flexDirection:'row', alignItems:'flex-end'}}>
+      <Text style={{paddingLeft: 20, paddingTop: 30,fontSize: 25, fontWeight: '900', color: '#fff'}}>Settings</Text>
+      <FontAwesome
+        name='gear'
+        color='#034732'
+        size={20}
+        style={{marginLeft:5, marginBottom:5}}
+      />
+      </View>
+
+      <View style={{marginTop: 10, height:'90%',justifyContent:'space-between'}}>
+        <View>
       <TouchableOpacity
-        onPress={() => {console.log('change form control')}}
+        onPress={() => {changePassword()}}
         style={styles.button}
       >
-        <Text style={{fontWeight: 'bold', fontSize:20}}>
-            Change Location
+        <View style={[styles.textBox, {borderTopWidth: 1}]}>
+        <Text style={styles.text}>
+            Email
         </Text>
+        </View>
       </TouchableOpacity>
+
 
       <TouchableOpacity
         onPress={() => {changePassword()}}
         style={styles.button}
       >
-        <Text style={{fontWeight: 'bold', fontSize:20}}>
-            Change Password
+        <View style={styles.textBox}>
+        <Text style={styles.text}>
+            Password
         </Text>
+        </View>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => {console.log('change form control')}}
+        style={styles.button}
+      >
+        <View style={styles.textBox}>
+        <Text style={styles.text}>
+            Location
+        </Text>
+        </View>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => {console.log('change form control')}}
+        style={styles.button}
+      >
+        <View style={styles.textBox}>
+        <Text style={styles.text}>
+            Hardiness Zone
+        </Text>
+        </View>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => {console.log('change form control')}}
+        style={styles.button}
+      >
+        <View style={styles.textBox}>
+        <Text style={styles.text}>
+            Unit System
+        </Text>
+        </View>
       </TouchableOpacity>
 
       <TouchableOpacity
         onPress={() => {firebase.auth().signOut()}}
         style={styles.button}
       >
-        <Text style={{fontWeight: 'bold', fontSize:20}}>
+        <View style={styles.textBox}>
+        <Text style={styles.text}>
+            Weather Alerts
+        </Text>
+        </View>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => {firebase.auth().signOut()}}
+        style={styles.button}
+      >
+        <View style={styles.textBox}>
+        <Text style={styles.text}>
+            Remind me about my plants
+        </Text>
+        </View>
+      </TouchableOpacity>
+
+      <View style={{flexDirection:'row', alignItems:'flex-end'}}>
+      <Text style={{paddingLeft: 20, paddingTop: 30,fontSize: 25, fontWeight: '900', color: '#fff'}}>Help</Text>
+      <FontAwesome
+      name='question-circle'
+      color={'#545B98'}
+      size={20}
+      style={{marginLeft: 5, marginBottom:3}}
+      />
+      </View>
+      <TouchableOpacity
+        onPress={() => {firebase.auth().signOut()}}
+        style={styles.button}
+      >
+        <View style={[styles.textBox, {borderTopWidth:1, marginTop: 10}]}>
+        <Text style={styles.text}>
+            Knowledge Base/FAQ
+        </Text>
+        </View>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => {firebase.auth().signOut()}}
+        style={styles.button}
+      >
+        <View style={styles.textBox}>
+        <Text style={styles.text}>
+            About Growr
+        </Text>
+        </View>
+      </TouchableOpacity>
+        </View>
+
+      <TouchableOpacity
+        onPress={() => {firebase.auth().signOut()}}
+        style={styles.button}
+      >
+        <View style={[styles.textBox, {borderBottomWidth:0, borderTopWidth:1, marginBottom: 10, flexDirection:'row', alignItems:'center'}]}>
+        <Text style={[styles.text, {color:'#fff', fontWeight:'900', fontSize: 25, paddingRight: 5}]}>
             Sign out
         </Text>
+        <FontAwesome
+        color='#F97068'
+        size='25'
+        name='sign-out'
+        />
+        </View>
       </TouchableOpacity>
+      </View>
+
+      </View>
+      </SafeAreaView>
     </View>
   )
 }
@@ -48,12 +166,23 @@ export default Settings
 
 const styles = StyleSheet.create({
   button: {
-    marginTop: 50,
-    height: 70,
-    width: 250,
-    backgroundColor: '#026efd',
-    alignItems: 'center',
-    justifyContent: 'center',
-    alignSelf: 'center',
-    borderRadius: 50,
-},})
+    // marginTop: 50,
+    // height: 70,
+    // width: 250,
+    // backgroundColor: '#026efd',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    // alignSelf: 'center',
+    // borderRadius: 50,
+  },
+  text: {
+    color:'white',
+    fontSize:16,
+    paddingLeft: 20
+  },
+  textBox: {
+    paddingVertical:10,
+    borderBottomWidth: 1,
+    borderColor: 'rgba(255,255,255,.25)'
+  }
+})
